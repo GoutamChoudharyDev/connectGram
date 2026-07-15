4b91e9d22b443baefb6f0baa9072ff5626d354c8
GoutamChoudharyDev <rc.goutam.choudhary@gmail.com>
2026-07-15 14:41:35 +0530

    Profile update api

--- server/src/modules/profile/profile.controller.ts
+++ server/src/modules/profile/profile.controller.ts
@@ -17,6 +17,15 @@ export const getUserProfile = asyncHandler(async (req: Request, res: Response) =
         )
     }
 
+    if (!username.trim()) {
+        return sendResponse(
+            res,
+            400,
+            false,
+            "Username is required"
+        )
+    }
+
     // get user from database
     const user = await userRepository.findOne({
         where: { username }
@@ -48,6 +57,86 @@ export const getUserProfile = asyncHandler(async (req: Request, res: Response) =
         200,
         true,
         "User profile fetched successfully",
+        profile
+    )
+})
+
+// update user profile controller
+export const updateUserProfile = asyncHandler(async (req: Request, res: Response) => {
+    // get fullName, bio, website from req.body
+    const { fullName, bio, website } = req.body;
+
+    // get logged user
+    const user = req.user;
+
+    // validation
+    if (
+        fullName === undefined &&
+        bio === undefined &&
+        website === undefined
+    ) {
+        return sendResponse(
+            res,
+            400,
+            false,
+            "Provide atleast one field to update"
+        )
+    }
+
+    // full name validation 
+    if (fullName && fullName.length > 100) {
+        return sendResponse(
+            res,
+            400,
+            false,
+            "Full name can't exceed 100 character"
+        )
+    }
+
+    // bio validation
+    if (bio && bio.length > 150) {
+        return sendResponse(
+            res,
+            400,
+            false,
+            "Bio can't exced 150 characters"
+        )
+    }
+
+    // update fileds 
+    if (fullName !== undefined) {
+        user.fullName = fullName.trim();
+    }
+
+    if (bio !== undefined) {
+        user.bio = bio;
+    }
+
+    if (website !== undefined) {
+        user.website = website;
+    }
+
+    // save updated user
+    const updatedUser = await userRepository.save(user);
+
+    // public response
+    const profile = {
+        id: updatedUser.id,
+        username: updatedUser.username,
+        fullName: updatedUser.fullName,
+        profilePicture: updatedUser.profilePicture,
+        bio: updatedUser.bio,
+        website: updatedUser.website,
+        isVerified: updatedUser.isVerified,
+        createdAt: updatedUser.createdAt,
+        updatedAt: updatedUser.updatedAt
+    };
+
+    return sendResponse(
+        res,
+        200,
+        true,
+        "Profile Updated successfully",
         profile
     )
 })
--- server/src/modules/profile/profile.routes.ts
+++ server/src/modules/profile/profile.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getUserProfile } from "./profile.controller.js";
+import { getUserProfile, updateUserProfile } from "./profile.controller.js";
+import { isAuth } from "../../middleware/auth.middleware.js";
 
 const profileRouter = Router();
 
 // profile api's
 profileRouter.get("/:username", getUserProfile);
+profileRouter.patch("/", isAuth, updateUserProfile);
 
 // export 
 export default profileRouter;
